654bd3f75413f6d26d0d774c638caa4a8fc260a6
José Luis <jose.pintor@globallogic.com>
2019-04-15 14:23:45 -0300

    First Commit

---
+++ docker-sample_container/src/test/java/com/globallogic/microservices/dockersample/DockerSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.globallogic.microservices.dockersample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
